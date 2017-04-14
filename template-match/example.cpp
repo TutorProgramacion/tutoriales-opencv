b467cd4c74b892b113bc6ab8adce6e997bff6f24
Carmelo Marín Abrego <marin.carmelo.abrego@gmail.com>
2017-04-14 09:58:53 -0600

    Búsqueda de patrones (actualización)
    Actualizando el tutorial búsqueda de patrones aplicando template
    matching con OpenCV 3.x

--- template-match/example.cpp
+++ template-match/example.cpp
@@ -6,11 +6,12 @@ using namespace cv;
 
 int main(int, char** argv)
 {
-    const char* image_window = "Source Image";
+    const char* image_window = "Busqueda de patrones";
     const char* result_window = "Result window";
 
-    Mat img_src = imread("image/messi.jpg", 1);
-    Mat templ = imread("image/templ.jpg", 1);
+    Mat img_src = imread("data/images/image.png",  CV_LOAD_IMAGE_COLOR);
+    Mat templ = imread("data/images/banana.png", CV_LOAD_IMAGE_COLOR);
+    Mat mask = imread("data/images/mascara.png", CV_LOAD_IMAGE_COLOR);
 
     // obtener las dimensiones para el cv::Mat resultado
     int result_cols = img_src.cols - templ.cols + 1;
@@ -19,10 +20,10 @@ int main(int, char** argv)
     Mat result(result_rows, result_cols, CV_32FC1);
 
     // establecer el match template mode
-    cv::TemplateMatchModes match_mode = cv::TemplateMatchModes::TM_CCOEFF_NORMED;
+    cv::TemplateMatchModes match_mode = cv::TemplateMatchModes::TM_SQDIFF;
 
     // aplicar match template con el modo indicado y normalizar el resultado
-    matchTemplate(img_src, templ, result, match_mode);
+    matchTemplate(img_src, templ, result, match_mode, mask);
     normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
 
     double minVal, maxVal;
@@ -40,10 +41,13 @@ int main(int, char** argv)
     rectangle(img_src, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar(0, 255, 0), 2, CV_AA, 0);
 
     // mostrar las imagenes
-    imshow(result_window, result);
     imshow(image_window, img_src);
 
+    imshow("MASCARA", mask);
+    imshow("PATRON", templ);
+
     waitKey(0);
+
     return 0;
 }
 
